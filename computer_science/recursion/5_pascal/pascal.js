ecde4e91ba628d2836a285f9c5164d6b6e37e6dd
Dani-Carmi <danielecarminati.dc@gmail.com>
2026-01-12 18:51:44 +0100

    pascal: done

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,16 @@
-const pascal = function() {
-  
+const pascal = function (rowNum) {
+  if (rowNum === 1) return [1];
+
+  let currentRow = [1];
+
+  const previousRow = pascal(rowNum - 1);
+  previousRow.forEach((number, i) => {
+    const rightNum = previousRow[i + 1] ?? 0;
+    currentRow.push(number + rightNum);
+  });
+
+  return currentRow;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
