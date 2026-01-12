f34b6a494a7cee97c811492dcfffc5485f2a425b
Dani-Carmi <danielecarminati.dc@gmail.com>
2026-01-12 14:08:21 +0100

    factorial: done

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,13 @@
-const factorial = function() {
+const factorial = function (n) {
+  if (n === 0) {
+    return 1;
+  }
 
+  if (n < 0 || !Number.isInteger(n)) {
+    return undefined;
+  }
+
+  return (n *= factorial(n - 1));
 };
 
 // Do not edit below this line
