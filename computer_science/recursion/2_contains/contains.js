9e77735e830add92e5077b97cd771a9a30b0d92f
Dani-Carmi <danielecarminati.dc@gmail.com>
2026-01-12 16:44:31 +0100

    contains: done

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,16 @@
-const contains = function() {
-  
+const contains = function (obj, searchValue) {
+  const values = Object.values(obj);
+
+  if (values.includes(searchValue)) return true;
+
+  const nestedObjects = values.filter(
+    (value) => typeof value === "object" && value !== null
+  );
+
+  return nestedObjects.some((nestedObject) =>
+    contains(nestedObject, searchValue)
+  );
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
